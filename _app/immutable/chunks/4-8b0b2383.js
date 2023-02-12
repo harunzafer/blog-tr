f6687d82087b200f55b453f12fb7fab9c96a4809
Harun Zafer <harunzafer.dev@gmail.com>
2023-02-12 01:13:47 -0500

    Updates

---
+++ _app/immutable/chunks/4-8b0b2383.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/hakkinda/_page.md-2b86c356.js";export{t as component};
---
+++ _app/immutable/modules/pages/_slug_/_page.js-d0d6c49a.js
@@ -0,0 +1,1 @@
+import"../../../chunks/preload-helper-41c905a7.js";import{l}from"../../../chunks/_page-fc9910f0.js";export{l as load};
