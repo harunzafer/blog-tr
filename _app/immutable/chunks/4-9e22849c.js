90e40c1c6bf2b48474f02da11f11f5060bae3d22
Harun Zafer <harunzafer.dev@gmail.com>
2023-02-26 16:09:18 -0500

    Updates

---
+++ _app/immutable/chunks/4-9e22849c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/demo/_page.svelte-59bcd773.js";export{t as component};
---
+++ _app/immutable/components/pages/demo/_page.svelte-59bcd773.js
@@ -0,0 +1,1 @@
+import{S as e,i as t,s as a}from"../../../chunks/index-c3577ca2.js";class o extends e{constructor(s){super(),t(this,s,null,null,a,{})}}export{o as default};
